38485c0e371f8bf024f304eaa529b5181ba8970c
DXF0530 <dmitry.fleish@sbdinc.com>
2023-03-01 13:06:01 +0200

    fixed

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import {RouterModule} from '@angular/router';
 import {Component} from '@angular/core';
 import {LibAOneComponent} from '@angular-folders/lib-a/lib-a-one';
-import {LibBOneComponent} from '@angular-folders/lib-b/lib-b-one';
-import {LibBTwoComponent} from '@angular-folders/lib-b/lib-b-two';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, LibAOneComponent, LibBOneComponent, LibBTwoComponent],
+  imports: [RouterModule, LibAOneComponent],
   selector: 'angular-folders-root',
-  templateUrl: './app.component.html',
+  template: `
+    <router-outlet></router-outlet>
+    <angular-folders-lib-a-one/>
+
+  `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
---
+++ libs/lib-b/lib-b-one/index.ts
@@ -0,0 +1,1 @@
+export * from './src/lib/lib-b-one.component';
